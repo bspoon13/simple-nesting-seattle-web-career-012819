c8fc506e6b5c98bfc1d65c36c4421ff3d973f247
Billy Witherspoon <wwither@gmail.com>
2019-01-23 02:48:03 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -37,7 +37,6 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
-
 	programmer_hash[:alan_kay][:known_for]
 end
 
@@ -59,7 +58,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-	programmer_hash[:dennis_ritchie][:languages]
+	programmer_hash[:dennis_ritchie][:languages].join("")
 end
 
 def adding_matz

37a0b11c049fc7a8c68917a947353cd816d25cc2
Billy Witherspoon <wwither@gmail.com>
2019-01-23 02:57:53 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -126,6 +126,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-	programmer_hash
+	existing_languages = programmer_hash[:dennis_ritchie][:languages].join(" ")
+	programmer_hash[:dennis_ritchie][:languages] = [existing_languages, "Assembly"]
 
 end
